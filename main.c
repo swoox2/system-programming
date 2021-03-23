11a23f1606f9030b9de9a6aa2d627936b49b2d57
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-03-23 23:55:03 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+	int i; struct stat file_info; mode_t file_mode;
+
+	for(i=1 ; i<argc ; i++){
+		if(stat(argv[i], &file_info)<0){
+			perror("stat :");
+			exit(1);
+		}
+		file_mode = file_info.st_mode;
+
+		if(S_ISDIR(file_mode)){
+			if(chmod(argv[i], S_IRUSR |S_IXUSR |S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) < 0){
+				perror("chmod :");
+				exit(1);
+		}
+		}
+		if(S_ISREG(file_mode)){
+			//printf("mode = %o \n",file_mode);
+			if( ( (file_mode & S_IXUSR) || (file_mode & S_IXGRP) || (file_mode & S_IXOTH) ) !=0 ){
+				chmod(argv[i], S_IRUSR |S_IXUSR |S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+				//printf("execute yes \n");
+			}
+			else{
+				chmod(argv[i], S_IRUSR | S_IRGRP | S_IROTH);
+				//printf("execute no \n");
+			}
+		}
+	}
+	 return 0;
+}
